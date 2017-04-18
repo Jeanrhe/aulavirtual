4850c31047af196de0ecd8d05040d3ce0969fb8f
lupe58ex <lu.58ex@gmail.com>
2017-04-18 18:54:07 -0500

    dd index

--- index.php
+++ index.php
@@ -1,13 +1,1 @@
-<<<<<<< HEAD
-"hola mundo y ro"
-=======
-<!DOCTYPE html>
-<html>
-<head>
-	<title>prueba</title>
-</head>
-<body><h1>Esto es una prueba</h1>
-
-</body>
-</html>
->>>>>>> origin/master
+hey
